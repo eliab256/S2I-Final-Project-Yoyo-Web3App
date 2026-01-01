44c0bace3b313ec54a5bbbe3ecbca923177f7d2f
eliab256 <bordonielia96@gmail.com>
2026-01-01 19:53:54 +0100

    deleted double ownable import

--- foundry/test/Mocks/YoyoNftFailingMintMock.sol
+++ foundry/test/Mocks/YoyoNftFailingMintMock.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import { ERC721 } from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 import { Strings } from '@openzeppelin/contracts/utils/Strings.sol';
-import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 import { ConstructorParams } from '../../src/YoyoTypes.sol';
 import '../../src/YoyoNft/YoyoNftErrors.sol';
 import '../../src/YoyoNft/YoyoNftEvents.sol';
